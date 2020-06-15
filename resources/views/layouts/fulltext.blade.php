e3a72213f808b621325bdf100c8058f4bd3597e3
Jon Fackrell <support@refbytes.com>
2020-06-15 15:41:44 -0600

    no message

--- resources/views/layouts/fulltext.blade.php
+++ resources/views/layouts/fulltext.blade.php
@@ -325,6 +325,24 @@
             /** **/
         });
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-886315-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-886315-1');
+    </script>
+    <script type='text/javascript'>
+        window.__lo_site_id = 115865;
+
+        (function() {
+            var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+            wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+        })();
+    </script>
     <script>
         window.onUsersnapCXLoad = function(api) {
             api.init({
