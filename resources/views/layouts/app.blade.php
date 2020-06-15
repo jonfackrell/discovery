caf8975910776e7c56c3bd6c35731eb6d2e6a0db
Jon Fackrell <support@refbytes.com>
2020-06-15 14:33:11 -0600

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -408,7 +408,20 @@
             /** **/
         });
     </script>
-
+    <script>
+        window.onUsersnapCXLoad = function(api) {
+            api.init({
+                user: {
+                    user_id: "{{ optional(auth()->user())->id }}", // Id of the user
+                    email: "{{ optional(auth()->user())->email }}" // Email address
+                }
+            });
+        }
+        var script = document.createElement('script');
+        script.defer = 1;
+        script.src = 'https://widget.usersnap.com/global/load/f0632680-0847-4e59-a7b5-88a1f9adf57d?onload=onUsersnapCXLoad';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    </script>
 
 </body>
 </html>
